155afbb61aeff2fc5fe20f312fb95d3dab5d5ec2
Mattabyte <matthew.inger@intelligentpathways.com.au>
2025-02-04 05:15:30 +1000

    Added S3 node to mapping.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 from .nodes.secure_api_call import SecureApiCall
 from .nodes.secure_api_call_aws import SecureApiCallAws
+from .nodes.save_video_files_s3 import SaveVideoFilesS3
 
 NODE_CLASS_MAPPINGS = {
     "SecureAPI-SecureAPI": SecureApiCall,
-    "SecureAPI-SecureAPI-AWS": SecureApiCallAws
+    "SecureAPI-SecureAPI-AWS": SecureApiCallAws,
+    "SaveVideoFilesS3": SaveVideoFilesS3
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SecureAPI-SecureAPI": "Secure API Call",
-    "SecureAPI-SecureAPI-AWS": "Secure API Call (AWS)"
+    "SecureAPI-SecureAPI-AWS": "Secure API Call (AWS)",
+    "SaveVideoFilesS3": "Save Video Files to S3"
 }
 
