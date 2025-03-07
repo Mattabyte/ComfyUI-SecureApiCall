f40c0e8c024fa89bc87ab43ca52b97c61922876f
Mattabyte <matthew.inger@intelligentpathways.com.au>
2025-03-08 09:13:31 +1000

    Add SaveLatentToS3 node

--- __init__.py
+++ __init__.py
@@ -1,16 +1,18 @@
 from .nodes.secure_api_call import SecureApiCall
 from .nodes.secure_api_call_aws import SecureApiCallAws
 from .nodes.save_video_files_s3 import SaveVideoFilesS3
-
+from .nodes.save_latent_s3 import SaveLatentToS3
 NODE_CLASS_MAPPINGS = {
     "SecureAPI-SecureAPI": SecureApiCall,
     "SecureAPI-SecureAPI-AWS": SecureApiCallAws,
-    "SaveVideoFilesS3": SaveVideoFilesS3
+    "SaveVideoFilesS3": SaveVideoFilesS3,
+    "SaveLatentToS3": SaveLatentToS3
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SecureAPI-SecureAPI": "Secure API Call",
     "SecureAPI-SecureAPI-AWS": "Secure API Call (AWS)",
-    "SaveVideoFilesS3": "Save Video Files to S3"
+    "SaveVideoFilesS3": "Save Video Files to S3",
+    "SaveLatentToS3": "Save Latent to S3"
 }
 
